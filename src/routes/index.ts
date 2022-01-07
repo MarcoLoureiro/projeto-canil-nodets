e6ef48fe1b9680189032bd432bf26cd300018633
Marco <marcoantonioloureiro098@gmail.com>
2022-01-07 03:34:08 -0300

    Projeto finalizado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import * as PageController from '../controllers/pageController';
 import * as SearchController from '../controllers/searchController';
 const router = Router();
 
-router.get('/',PageController.home);
+router.get('/home',PageController.home);
 router.get('/dogs',PageController.dogs);
 router.get('/cats',PageController.cats);
 router.get('/fishes',PageController.fishes);
